374d41957a05bf193b4f9fd963554a5684fe64d5
Os <oswaldrijo@Oswalds-Mac-Home.local>
2024-08-18 10:57:06 -0300

    chore: improve dir naming for js apops

---
+++ src/javascript/pb/src/index.ts
@@ -0,0 +1,6 @@
+export * from './google/protobuf/timestamp';
+export * from './index';
+export * from './operations/operations_service';
+export * from './operations/structures';
+export * from './users/structures';
+export * from './users/users_service';

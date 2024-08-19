42a4b0abc5fba81800c03d88451b1992c6711650
Os <oswaldrijo@Oswalds-Mac-Home.local>
2024-08-18 22:25:15 -0300

    chore: improve kill-app script

--- scripts/kill-app.sh
+++ scripts/kill-app.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
-docker-compose -f docker/operations_service/docker-compose.yaml down
-docker-compose -f docker/users_service/docker-compose.yaml down
-docker-compose -f docker/public_api/docker-compose.yaml down
-docker-compose -f docker/web_client/docker-compose.yaml down
-docker-compose -f docker/database/docker-compose.yaml down
+OS="$(uname -s | tr '[:upper:]' '[:lower:]')"
+COMPOSE=""
+if [ "$OS" = "linux" ]; then
+  COMPOSE="docker compose"
+  sudo systemctl stop postgresql
+else
+  COMPOSE="docker-compose"
+fi
+
+$COMPOSE -f docker/operations_service/docker-compose.yaml down
+$COMPOSE -f docker/users_service/docker-compose.yaml down
+$COMPOSE -f docker/public_api/docker-compose.yaml down
+$COMPOSE -f docker/web_client/docker-compose.yaml down
+$COMPOSE -f docker/database/docker-compose.yaml down
 docker rmi public_api_tn_public_api
 docker rmi operations_service_users_service
 docker rmi operations_service_operations_service
